7ee4aa21d6e0bf6e2ed0ada16e15f48fabd26649
Suyeon Jung <suyeonjungdev@gmail.com>
2024-07-30 14:01:40 +0900

    siteRoot: '/litmus-api-docs/docs'

--- docs/_app/immutable/entry/start.B4cAOMbP.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Ci1bwonD.js";export{t as start};
--- magidoc.mjs
+++ magidoc.mjs
@@ -42,7 +42,7 @@ export default {
              *
              * @default undefined
              */
-            siteRoot: '/litmus-api-docs',
+            siteRoot: '/litmus-api-docs/docs',
 
             /**
              * Customizes the website meta tags in the header of the HTML pages.
