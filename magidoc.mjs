dac36e75537c107df1c3bfbaab1678fc8d4aa4ab
Suyeon Jung <suyeonjungdev@gmail.com>
2024-07-30 09:40:40 +0900

    change sireRoot to /litmus-api-docs

--- magidoc.mjs
+++ magidoc.mjs
@@ -35,7 +35,7 @@ export default {
              *
              * @default undefined
              */
-            siteRoot: '/litmus-api-docs/docs',
+            siteRoot: '/litmus-api-docs',
 
             /**
              * Customizes the website meta tags in the header of the HTML pages.
