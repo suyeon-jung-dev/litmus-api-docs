f1dd42f6fa6dc9b3039ad0864745433b6adc7479
Suyeon Jung <suyeonjungdev@gmail.com>
2024-08-04 16:27:59 +0900

    Revert "test: changed to siteRoot: '/litmus-api-docs/v3.9.1',"
    This reverts commit c8172ae415ed343a72754e08b757024e861e6af4.

--- magidoc.mjs
+++ magidoc.mjs
@@ -42,7 +42,7 @@ export default {
              *
              * @default undefined
              */
-            siteRoot: '/litmus-api-docs/v3.9.1',
+            siteRoot: '/litmus-api-docs',
 
             /**
              * Customizes the website meta tags in the header of the HTML pages.
